f75ba3843d1f0cbb838bb2e33c8fa020330241a7
Gene <genewang7@gmail.com>
2023-05-24 21:33:27 +0800

    feat: kafka-sink-connect success

--- kafka scripts/kafka-connect.sh
+++ kafka scripts/kafka-connect.sh
@@ -1,3 +1,3 @@
 cd ~
 cd kafka_2.13-3.4.0
-nohup bin/connect-distributed.sh
+nohup bin/connect-distributed.sh config/connect-distributed.properties
---
+++ mongo.Dockerfile
@@ -0,0 +1,3 @@
+FROM mongo:6.0.5
+
+ADD connector-config /connector-config
