a88ff7323daf4f04f42959d6fb8c314f12e37a4e
Gene <genewang7@gmail.com>
2023-07-18 15:10:45 +0800

    feat: kubernetes volume

--- src/main/java/com/chunhanwang/kafka/KafkaTopicConfig.java
+++ src/main/java/com/chunhanwang/kafka/KafkaTopicConfig.java
@@ -1,14 +1,14 @@
-package com.chunhanwang.kafka;
-
-import org.apache.kafka.clients.admin.*;
-import org.springframework.context.annotation.*;
-import org.springframework.kafka.config.*;
-
-@Configuration
-public class KafkaTopicConfig {
-    @Bean
-    public NewTopic ebankTopic() {
-        return TopicBuilder.name("ebankTopic-filtered")
-                .build();
-    }
-}
+//package com.chunhanwang.kafka;
+//
+//import org.apache.kafka.clients.admin.*;
+//import org.springframework.context.annotation.*;
+//import org.springframework.kafka.config.*;
+//
+//@Configuration
+//public class KafkaTopicConfig {
+//    @Bean
+//    public NewTopic ebankTopic() {
+//        return TopicBuilder.name("ebankTopic-filtered")
+//                .build();
+//    }
+//}
